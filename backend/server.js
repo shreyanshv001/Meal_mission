6225fc1fbd259750db1606b8ad75925489d51b65
Shreyansh Verma <161208039+shreyanshv001@users.noreply.github.com>
2025-08-28 02:41:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 app.use(
   cors({
-    origin: "http://localhost:5173", // Your frontend URL
+    origin: "meal-mission-9cy8gagf9-shreyansh-vermas-projects.vercel.app", // Your frontend URL
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
