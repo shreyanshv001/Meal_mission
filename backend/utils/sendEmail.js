563b88c980ebfc3d35aeb6021a6eba17501d9ac7
Shreyansh Verma <161208039+shreyanshv001@users.noreply.github.com>
2025-10-05 05:13:44 +0530

    port updated

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,6 +4,7 @@ const sendEmail = async (to, subject, text) => {
     try{
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
+            port: 587,
             secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
